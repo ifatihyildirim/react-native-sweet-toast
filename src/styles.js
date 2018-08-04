27783013a39b8fd02fa96051ade0fd26754a71d7
Fatih Yıldırım <isafatihyildirim@gmail.com>
2018-08-04 21:03:22 +0300

    added style file

---
+++ src/styles.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+const Styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    minHeight: 60,
+    padding: 10,
+    backgroundColor: '#eaeaeb',
+    borderRadius: 20,
+    left: 10,
+    right: 10,
+  },
+});
+
+export default Styles;
