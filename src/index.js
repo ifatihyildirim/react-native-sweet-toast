80c63b6ff4c2e5d3433e138c2e86eeee91d7d2d9
Fatih Yıldırım <isafatihyildirim@gmail.com>
2018-08-04 21:03:31 +0300

    edited index file

--- src/index.js
+++ src/index.js
@@ -4,99 +4,134 @@
  *
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Animated } from 'react-native';
+import React, { Component } from 'react';
+import { Animated } from 'react-native';
+import PropTypes from 'prop-types';
 
-class Alert extends Component {
-    constructor(props) {
-        super(props)
+import Styles from './styles';
 
-        this.state = {
-            containerHeight: 60,
-        }
-        this.animatedValue = new Animated.Value(0)
+class SweetToast extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      containerHeight: 60,
+    };
+    this.animatedValue = new Animated.Value(0);
+  }
+
+  componentDidMount() {
+    this.props.onRef(this);
+  }
+
+  componentWillUnmount() {
+    this.props.onRef(undefined);
+  }
+
+  callToast() {
+    Animated.timing(
+      this.animatedValue, {
+        toValue: 100,
+        duration: 350,
+      },
+    ).start(this.flash());
+  }
+
+  closeToast() {
+    Animated.timing(
+      this.animatedValue, {
+        toValue: -100,
+        duration: 350,
+      },
+    ).start();
+  }
+
+  flash() {
+    const {
+      flash,
+      flashTime,
+    } = this.props;
+
+    setTimeout(() => {
+      if (flash) {
+        return this.closeToast();
+      }
+      return null;
+    }, flashTime);
+  }
+
+  initialRange(position, positionValue) {
+    const {
+      containerHeight,
+    } = this.state;
+    const value = positionValue + containerHeight;
+
+    if (position === 'top') {
+      return -Math.abs(value);
     }
+    return Math.abs(value);
+  }
 
-    componentDidMount() {
-        this.props.onRef(this)
-      }
-
-      componentWillUnmount() {
-        this.props.onRef(undefined)
-      }
-
-      callToast() {
-        Animated.timing(
-          this.animatedValue,
-          { 
-            toValue: 100,
-            duration: 350
-          }).start(this.flash())
-      }
-      
-      closeToast() {
-          Animated.timing(
-          this.animatedValue,
-          { 
-            toValue: -100,
-            duration: 350
-          }).start()
-      }
-
-      flash(){
-        setTimeout(() => {
-          if (this.props.flash) {
-              return this.closeToast();
-          }
-        }, 2000);
-    }
-
-      initialRange = (position, positionValue) => {
-          const value =  positionValue + this.state.containerHeight;
-          if (position === 'top') {
-              return -Math.abs(value);
-          }
-
-          return Math.abs(value);
-      }
-    
   render() {
-    const {position, positionValue, style} = this.props;
-    const outputRange =  [ this.initialRange(position, positionValue), 0 ]
-    
-    let animation = this.animatedValue.interpolate({
-        inputRange: [0, 100],
-        outputRange: outputRange
-      });
+    const {
+      position,
+      positionValue,
+      style,
+      children,
+    } = this.props;
+    const outputRange = [this.initialRange(position, positionValue), 0];
+    const animation = this.animatedValue.interpolate({
+      inputRange: [0, 100],
+      outputRange,
+    });
 
     return (
-        <Animated.View 
-        onLayout={(event) => {
-            const {height} = event.nativeEvent.layout;
-            this.setState({containerHeight: height});
-        }}
-        style={[
-            { transform: [{ translateY: animation}] },
-            {[position]: positionValue},
-            styles.container,
-            style,
-        ]}>
-            {this.props.children}
-        </Animated.View >
+      <Animated.View
+        onLayout={
+        (event) => {
+          const {
+            height,
+          } = event.nativeEvent.layout;
+          this.setState({
+            containerHeight: height,
+          });
+        }
+      }
+        style={
+        [{
+          transform: [{
+            translateY: animation,
+          }],
+        },
+        {
+          [position]: positionValue,
+        },
+        Styles.container,
+        style,
+        ]
+      }
+      >
+        {children}
+      </Animated.View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-      position: 'absolute',
-      minHeight: 60,
-      padding: 10,
-      backgroundColor: '#eaeaeb',
-      borderRadius: 20,
-      left: 10,
-      right: 10,
-    },
-});
+SweetToast.propTypes = {
+  flash: PropTypes.bool,
+  flashTime: PropTypes.number,
+  position: PropTypes.string,
+  positionValue: PropTypes.number,
+  style: PropTypes.shape(),
+  children: PropTypes.element.isRequired,
+  onRef: PropTypes.node.isRequired,
+};
 
-export default Alert
+SweetToast.defaultProps = {
+  flash: false,
+  flashTime: 1000,
+  position: 'bottom',
+  positionValue: 20,
+  style: {},
+};
+export default SweetToast;
